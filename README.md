40f50ae29ff584940ace443cf0cd193be8cab6e2
Fabian Enrique Gutierrez <fega.hg@gmail.com>
2017-04-08 22:24:04 -0500

    First commit Added Aqi.aqi() and Aqi.aqiArray()

---
+++ README.md
@@ -0,0 +1,29 @@
+# aqi-plus [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+> A package to calculate the air quality index from AirCast and NowCast
+
+## Installation
+
+```sh
+$ npm install --save aqi-plus
+```
+
+## Usage
+
+```js
+var aqiPlus = require('aqi-plus');
+
+aqiPlus('Rainbow');
+```
+## License
+
+MIT © [Fabian Enrique Gutierrez](http://fabiangutierrez.co)
+
+
+[npm-image]: https://badge.fury.io/js/aqi-plus.svg
+[npm-url]: https://npmjs.org/package/aqi-plus
+[travis-image]: https://travis-ci.org/fega/aqi-plus.svg?branch=master
+[travis-url]: https://travis-ci.org/fega/aqi-plus
+[daviddm-image]: https://david-dm.org/fega/aqi-plus.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/fega/aqi-plus
+[coveralls-image]: https://coveralls.io/repos/fega/aqi-plus/badge.svg
+[coveralls-url]: https://coveralls.io/r/fega/aqi-plus
---
+++ test/aqi-aqi.js
@@ -0,0 +1,44 @@
+/* eslint-env node, mocha */
+import aircast from '../lib/lib/aqi'
+const expect = require('chai').expect
+
+describe('aircast(good, undefined)', function () {
+  it('Shoud throw an error', function (done) {
+    expect(aircast.bind(this, 1)).to.throw(/pollutant/)
+    expect(aircast.bind(this, 1, 'pm1024h')).to.throw(/pollutant/)
+    expect(aircast.bind(this, 1, 'PM25_24h')).to.throw(/pollutant/)
+    expect(aircast.bind(this, 1, 'o3_8')).to.throw(/pollutant/)
+    done()
+  })
+})
+
+describe('aircast(good, good)', function () {
+  it('Shoud calculate the USA AQI', function (done) {
+    expect(aircast(150, 'o3_8h')).to.equal(247) // ok
+    expect(aircast(150, 'o3_1h')).to.equal(132) // ok
+    expect(aircast(150, 'pm10_24h')).to.equal(98)// ok
+    expect(aircast(150, 'pm25_24h')).to.equal(199)// ok
+    expect(aircast(15, 'co_8h')).to.equal(193) // ok
+    expect(aircast(15, 'so2_24h')).to.equal(null) // ok
+    expect(aircast(150, 'so2_24h')).to.equal(null) // ok
+    expect(aircast(350, 'so2_24h')).to.equal(215) // ok
+    expect(aircast(150, 'so2_1h')).to.equal(134) // ok
+    expect(aircast(150, 'no2_1h')).to.equal(110) // ok
+
+    expect(aircast(0, 'o3_1h')).to.equal(null)
+    expect(aircast(-1, 'o3_8h')).to.equal(null)
+    expect(aircast(null, 'o3_8h')).to.equal(null)
+    expect(aircast(1000, 'o3_8h')).to.equal(null)
+    done()
+  })
+})
+describe('aircast(edge cases, good)', function () {
+  it('Shoud evaluate edge cases', function (done) {
+    expect(aircast(0, 'o3_8h')).to.equal(0)
+    expect(aircast(-1, 'o3_8h')).to.equal(null)
+    expect(aircast(null, 'o3_8h')).to.equal(null)
+    expect(aircast(1000, 'o3_8h')).to.equal(null)
+    expect(aircast(undefined, 'o3_8h')).to.equal(null)
+    done()
+  })
+})
